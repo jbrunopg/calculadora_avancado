45135426c5efc89c798376447c6e1faa14f87a23
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-04 22:12:40 -0300

    Add files via upload

---
+++ calculador avançada.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+cor1 = "#2596be"
+
+janela =Tk()
+janela.title("Calculadora")
+janela.geometry("235*318")
+
+frame_tela = Frame(janela, width=235, height= 50,bg=cor1)
+frame_tela.grid = (row=0, column=0)
+
+janela.mainloop()
+
