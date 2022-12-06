ccb00deea2a40ddba56f148e4abf95264b09b5d9
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-06 15:55:13 -0300

    Update calculador avançada.py

--- calculador avançada.py
+++ calculador avançada.py
@@ -1,14 +1,27 @@
 from tkinter import *
 from tkinter import ttk
 
-cor1 = "#2596be"
+# cor
+cor1 = "#3b3b3b" #preto
+cor2 = "#feffff" #branco
+cor3 = "#38576b" #azul
+cor4 = "#ECEFF1" #cinza
+cor5 = "#FFAB40" #laranja
 
 janela =Tk()
 janela.title("Calculadora")
-janela.geometry("235*318")
+janela.geometry("235x318")
+janela.config(bg=cor1)
 
-frame_tela = Frame(janela, width=235, height= 50,bg=cor1)
-frame_tela.grid = (row=0, column=0)
+# criando frames
+frame_tela = Frame(janela, width=235, height=50,bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268,bg=cor1)
+frame_corpo.grid(row=0, column=0)
+
+#criando botões
+
+b_1 = Button(frame_corpo, text="C", width=11, height=2)
 
 janela.mainloop()
-
