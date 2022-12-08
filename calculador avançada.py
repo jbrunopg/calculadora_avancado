1499741c197447dc652b4113fc5d3c90fef90cab
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-07 23:10:45 -0300

    Update calculador avançada.py

--- calculador avançada.py
+++ calculador avançada.py
@@ -20,6 +20,10 @@ frame_tela.grid(row=0, column=0)
 frame_corpo = Frame(janela, width=235, height=268, bg=cor1)
 frame_corpo.grid(row=1, column=0)
 
+# criando label
+
+
+
 #criando botões
 
 b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
@@ -55,5 +59,12 @@ b_14 = Button(frame_corpo, text="3", width=5, height=2, bg=cor4, font=('Ivy 13 b
 b_14.place(x=118, y=156)
 b_15 = Button(frame_corpo, text="+", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_15.place(x=177, y=156)
+
+b_16 = Button(frame_corpo, text="0", width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_16.place(x=0, y=208)
+b_17 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_17.place(x=118, y=208)
+b_18 = Button(frame_corpo, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_18.place(x=177, y=208)
 
 janela.mainloop()
